aa907ba5764509cea963adc5db099e1bae2c5df7
XuanVuFsv <58604939+XuanVuFsv@users.noreply.github.com>
2023-10-05 20:12:47 +0700

    Fix some bug after revert
    Fix some bug after revert

--- Assets/Polygon Arsenal/Demo/Scripts/PolygonProjectileScript.cs
+++ Assets/Polygon Arsenal/Demo/Scripts/PolygonProjectileScript.cs
@@ -18,66 +18,66 @@ namespace PolygonArsenal
         {
             projectileParticle = Instantiate(projectileParticle, transform.position, transform.rotation) as GameObject;
             projectileParticle.transform.parent = transform;
-            //if (muzzleParticle)
-            //{
-            //    muzzleParticle = Instantiate(muzzleParticle, transform.position, transform.rotation) as GameObject;
-            //    Destroy(muzzleParticle, 1.5f); // Lifetime of muzzle effect.
-            //}
+            if (muzzleParticle)
+            {
+                muzzleParticle = Instantiate(muzzleParticle, transform.position, transform.rotation) as GameObject;
+                Destroy(muzzleParticle, 1.5f); // Lifetime of muzzle effect.
+            }
         }
 
-        //void FixedUpdate()
-        //{
-        //    RaycastHit hit;
+        void FixedUpdate()
+        {
+            RaycastHit hit;
 
-        //    float rad;
-        //    if (transform.GetComponent<SphereCollider>())
-        //        rad = transform.GetComponent<SphereCollider>().radius;
-        //    else
-        //        rad = colliderRadius;
+            float rad;
+            if (transform.GetComponent<SphereCollider>())
+                rad = transform.GetComponent<SphereCollider>().radius;
+            else
+                rad = colliderRadius;
 
-        //    Vector3 dir = transform.GetComponent<Rigidbody>().velocity;
-        //    if (transform.GetComponent<Rigidbody>().useGravity)
-        //        dir += Physics.gravity * Time.deltaTime;
-        //    dir = dir.normalized;
+            Vector3 dir = transform.GetComponent<Rigidbody>().velocity;
+            if (transform.GetComponent<Rigidbody>().useGravity)
+                dir += Physics.gravity * Time.deltaTime;
+            dir = dir.normalized;
 
-        //    float dist = transform.GetComponent<Rigidbody>().velocity.magnitude * Time.deltaTime;
+            float dist = transform.GetComponent<Rigidbody>().velocity.magnitude * Time.deltaTime;
 
-        //    if (Physics.SphereCast(transform.position, rad, dir, out hit, dist))
-        //    {
-        //        transform.position = hit.point + (hit.normal * collideOffset);
+            if (Physics.SphereCast(transform.position, rad, dir, out hit, dist))
+            {
+                transform.position = hit.point + (hit.normal * collideOffset);
 
-        //        GameObject impactP = Instantiate(impactParticle, transform.position, Quaternion.FromToRotation(Vector3.up, hit.normal)) as GameObject;
+                GameObject impactP = Instantiate(impactParticle, transform.position, Quaternion.FromToRotation(Vector3.up, hit.normal)) as GameObject;
 
-        //        if (hit.transform.tag == "Destructible") // Projectile will destroy objects tagged as Destructible
-        //        {
-        //            Destroy(hit.transform.gameObject);
-        //        }
+                if (hit.transform.tag == "Destructible") // Projectile will destroy objects tagged as Destructible
+                {
+                    Destroy(hit.transform.gameObject);
+                }
 
-        //        foreach (GameObject trail in trailParticles)
-        //        {
-        //            GameObject curTrail = transform.Find(projectileParticle.name + "/" + trail.name).gameObject;
-        //            curTrail.transform.parent = null;
-        //            Destroy(curTrail, 3f);
-        //        }
-        //        Destroy(projectileParticle, 3f);
-        //        Destroy(impactP, 5.0f);
-        //        Destroy(gameObject);
+                foreach (GameObject trail in trailParticles)
+                {
+                    GameObject curTrail = transform.Find(projectileParticle.name + "/" + trail.name).gameObject;
+                    curTrail.transform.parent = null;
+                    Destroy(curTrail, 3f);
+                }
+                Destroy(projectileParticle, 3f);
+                Destroy(impactP, 5.0f);
+                Destroy(gameObject);
 
-        //        ParticleSystem[] trails = GetComponentsInChildren<ParticleSystem>();
-        //        //Component at [0] is that of the parent i.e. this object (if there is any)
-        //        for (int i = 1; i < trails.Length; i++)
-        //        {
+                ParticleSystem[] trails = GetComponentsInChildren<ParticleSystem>();
+                //Component at [0] is that of the parent i.e. this object (if there is any)
+                for (int i = 1; i < trails.Length; i++)
+                {
 
-        //            ParticleSystem trail = trails[i];
+                    ParticleSystem trail = trails[i];
 
-        //            if (trail.gameObject.name.Contains("Trail"))
-        //            {
-        //                trail.transform.SetParent(null);
-        //                Destroy(trail.gameObject, 2f);
-        //            }
-        //        }
-        //    }
-        //}
+                    if (trail.gameObject.name.Contains("Trail"))
+                    {
+                        trail.transform.SetParent(null);
+                        Destroy(trail.gameObject, 2f);
+                    }
+                }
+            }
+        }
 
         //private bool hasCollided = false;
 
